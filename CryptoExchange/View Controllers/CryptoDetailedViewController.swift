9b7229b244ce47acc4c56bcc849fccddf86df600
shwaitkumar <shwaitkumar@icloud.com>
2022-01-03 18:25:04 +0530

    designed Coin details view controller

---
+++ CryptoExchange/View Controllers/CryptoDetailedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CryptoDetailedViewController.swift
+//  CryptoExchange
+//
+//  Created by Shwait Kumar on 03/01/22.
+//
+
+import UIKit
+
+class CryptoDetailedViewController : BaseUIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
